6ee41fa01e8cf948a786a3fc2bcb59c150dc41ce
aplicationdevelopment01 <157312042+aplicationdevelopment01@users.noreply.github.com>
2024-01-22 15:16:36 +0700

    Update Test Login

--- cypress/e2e/TestLoginBOCP.cy.js
+++ cypress/e2e/TestLoginBOCP.cy.js
@@ -4,13 +4,13 @@ describe('template spec', () => {
   })
   it('passes', () => {
     cy.get('input[id=Input_UsernameVal2]').type('sa1')
-  // {enter} causes the form to submit
+    // {enter} causes the form to submit
     const password = "123456"
     cy.get('input[id=Input_PasswordVal2]').type(`${password}{enter}`, { log: false })
     cy.url().should('include', '/')
     cy.viewport(1280, 720)
   })
-  it('Superadmin Has Manage', () => {
+  it('Superadmin Has Manage', () =>{
     cy.get('span').should('contain', "Manage")
   })
 })
